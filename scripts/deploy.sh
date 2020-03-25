f55a2756363130b19df69542a63746c4dfa6c244
Jisoo Byun <bjs1616@gmail.com>
2020-03-25 16:51:12 +0900

    deploy last test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ TOMCAT_VER=tomcat8
 sudo service $TOMCAT_VER stop
 sudo rm -rf /var/lib/$TOMCAT_VER/webapps/kt/
 sudo rm -rf /var/lib/$TOMCAT_VER/webapps/kt.war
-cp $REPOSITORY/zip/pro27-1.0.0-BUILD-SNAPSHOT.war /var/lib/$TOMCAT_VER/webapps/kt.war
+cp $REPOSITORY/zip/*.war /var/lib/$TOMCAT_VER/webapps/kt.war
 sudo service $TOMCAT_VER start
