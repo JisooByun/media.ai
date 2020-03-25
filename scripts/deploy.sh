8ce85a9e5fe33a784bcf0aa5fe87c1c916ab80a0
Jisoo Byun <bjs1616@gmail.com>
2020-03-25 16:19:20 +0900

    deploy test2

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/app/step2
+TOMCAT_VER=tomcat8
+
+sudo service $TOMCAT_VER stop
+sudo rm -rf /var/lib/$TOMCAT_VER/webapps/kt/
+sudo rm -rf /var/lib/$TOMCAT_VER/webapps/kt.war
+cp $REPOSITORY/zip/target/pro27-1.0.0-BUILD-SNAPSHOT.war /var/lib/$TOMCAT_VER/webapps/kt.war
+sudo service $TOMCAT_VER start
